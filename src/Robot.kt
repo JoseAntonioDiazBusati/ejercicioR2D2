3573fb292696b186132056d87cb1b05726f09399
JoseAntonio <jdiabus511@g.educaand.es>
2025-02-03 21:31:19 +0100

    r2d2 toString

--- src/Robot.kt
+++ src/Robot.kt
@@ -1,2 +1,14 @@
-class Robot (val nombre: String, posX: Int, posY: Int, var direccion: Direccion) {
+class Robot (val nombre: String) {
+
+    var posX: Int = 0
+    var posY: Int = 0
+    var direccion: Direccion = Direccion.POSITIVEY
+
+    fun mover(){}
+    fun obtenerPosicion(){}
+    fun obtenerDireccion(){}
+
+    override fun toString(): String {
+        return "$nombre está en ($posX, $posY) $direccion"
+    }
 }
